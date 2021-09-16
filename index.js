85783af1047d4d2fabbc8874c4a46fe935866466
tonyomendoza <mendozatony@outlook.com>
2021-09-15 21:40:05 -0700

    Lesson 5 & 7

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+const { response } = require('express');
 const express = require('express');
 
-const appp = express();
+const app = express();
 const port = 3000;
 
-appp.listen(port, () => {
+app.get('/', (request, response) => {
+    console.log(request);
+    response.send("Hello World");
+})
+
+app.get('/hello-world', (request, response) => {
+    console.log(request);
+    response.send("Hello World 2");
+})
+
+app.get('/test', (request, response) => {
+    console.log(request);
+    response.send("test");
+})
+
+app.listen(port, () => {
     console.log("App is running on PORT=" + port);
 })
