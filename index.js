ed1e2af643e58f1c23d1fc0c7b475daab7bb832d
tonyomendoza <mendozatony@outlook.com>
2021-09-21 13:51:45 -0700

    Lesson 4 Mongoose & Lesson 5 Mongo Authentication

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser'); // body parser should be passed down to modules such as main.js and password.js
 const cors = require('cors');
-const cookieParser = require('cookie-parser'); 
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const routes = require('./routes/main');
 const passwordRoutes = require('./routes/password');
+
+// Setup mongo connection
+const uri = process.env.MONGO_CONNECTION_URL || process.env.LOCAL_MONGO_CONNECTION_URL;
+const mongoConfig = {
+    // unsupported options in Mongoose 6.0 https://stackoverflow.com/questions/68958221/mongoparseerror-options-usecreateindex-usefindandmodify-are-not-supported
+    /*useNewUrlParser: true,
+    useCreateIndex: true,*/
+}
+if (!process.env.MONGO_CONNECTION_URL && process.env.MONGO_USERNMAME && process.env.MONGO_PASSWORD){
+    mongoConfig.auth = { authSource: 'admin' };
+    mongoConfig.user = process.env.MONGO_USERNMAME;
+    mongoConfig.pass = process.env.MONGO_PASSWORD;
+}
+mongoose.connect(uri, mongoConfig);
+
+mongoose.connection.on('error', (error)=>{
+    console.log(error);
+    process.exit(1);
+});
 
 const app = express();
 const port = process.env.PORT || 3000; // || can use the right hand side as a fallback
@@ -58,6 +78,10 @@ app.use((error, request, response, next) => {
     });
 });
 
-app.listen(port, () => {
-    console.log("App is running on PORT=" + port);
+
+mongoose.connection.on('connected', () => {
+    console.log('connected to mongo')
+    app.listen(port, () => {
+        console.log("App is running on PORT=" + port);
+    })
 })
