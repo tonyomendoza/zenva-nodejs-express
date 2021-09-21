f7dbd481c6a332737b52260de026b6df84648005
tonyomendoza <mendozatony@outlook.com>
2021-09-21 16:13:03 -0700

    Lesson 16 SMTP Object

--- routes/password.js
+++ routes/password.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const hbs = require('node-mailer-express-handlebars');
+const nodemailer = erquire('nodemailer');
+const path = require('path');
+
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+
+const smtpTransport = nodemailer.createTransport({
+    service: process.env.EMAIL_PROVIDER,
+    auth: {
+        user:email,
+        pass: password
+    }
+});
+
+const handlebarsOptions = {
+    viewEngine: {
+        extName: '.hbs',
+        partialsDir: './templates/',
+        layoutsDir: './templates/'
+    },
+    viewPath: path.resolve('./templates/'),
+    extName: '.html'
+}
+
+smptpTransport.use('compile', hbs(handlebarsOptions));
 
 router.post('/forgot-password', (request, response) => {
     if(!request.body || !request.body.email){
